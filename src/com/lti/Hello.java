82ef0148c836d7fe72f9dd70da2f1df721ada40a
musukha20 <musukhab2@gmail.com>
2020-09-14 12:10:53 +0530

    Added date in Hello.java

--- src/com/lti/Hello.java
+++ src/com/lti/Hello.java
@@ -1,9 +1,15 @@
 package com.lti;
 
+import java.util.Date;
+
 public class Hello {
 	
 	public void main() {
 		System.out.println("Hello");
+		
+		Date d =new Date();
+		System.out.println(d);
+		
 	}
 
 }
